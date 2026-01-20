eb1796e054a4149d672bf1d139388ea00c9ada6f
Maureen Njihia <maureennjihia468@gmail.com>
2026-01-20 18:54:08 +0300

    feat(home): integrate Hero component into the main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/ui/header";
+import Hero from "@/components/ui/hero";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
