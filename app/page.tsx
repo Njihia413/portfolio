ad9fe73cc2bae42cbc5e5cb0c79a14eed833c1ce
Maureen Njihia <maureennjihia468@gmail.com>
2026-01-20 20:42:17 +0300

    feat: integrate Facts section into Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/ui/header";
-import Hero from "@/components/ui/hero";
+import Hero from "@/components/hero";
+import Facts from "@/components/facts";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Facts />
     </>
   );
 }
