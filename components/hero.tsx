ea89ca7a9563cf2eef11ed74fed012fe4c527617
Maureen Njihia <maureennjihia468@gmail.com>
2026-01-20 20:41:51 +0300

    refactor: move and update Hero component to root components directory

---
+++ components/hero.tsx
@@ -0,0 +1,75 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import TyperComponent from "@/components/common/TyperComponent";
+
+export default function Hero() {
+  return (
+    <div className="rpp-banner-two-area">
+      <div className="container">
+        <div className="banner-two-main-wrapper">
+          <div className="row align-items-center">
+            <div className="col-lg-5 order-lg-2">
+              <div className="banner-right-content">
+                  <div className="bg-benner-img-six">
+                    <Image
+                      className="tmp-scroll-trigger tmp-zoom-in animation-order-1"
+                      alt="Maureen Njihia"
+                      src="/assets/images/maureen_njihia.png"
+                      width={531}
+                      height={531}
+                    />
+                  </div>
+              </div>
+            </div>
+            <div className="col-lg-7 order-lg-1 mt--100">
+              <div className="inner">
+                <span className="sub-title tmp-scroll-trigger tmp-fade-in animation-order-1">
+                  Hello
+                </span>
+                <h1 className="title tmp-scroll-trigger tmp-fade-in animation-order-2">
+                  I&apos;m Maureen Njihia, I <br />
+                  <span className="header-caption">
+                    <span className="cd-headline clip is-full-width">
+                      <TyperComponent
+                        strings={[
+                        "build seamless user experiences.",
+                        "create AI-powered solutions.",
+                        "optimize for peak performance.",
+                        "boost user adoption."
+                        ]}
+                      />
+                    </span>
+                  </span>
+                </h1>
+                <p className="disc tmp-scroll-trigger tmp-fade-in animation-order-3">
+                  A <span>Software Engineer </span> passionate about crafting high-performance applications that users love. 
+                  I combine strong sensibilities with full-stack expertise to build scalable solutions that drive <span>measurable </span> impact.
+                  
+                </p>
+                <div className="button-area-banner-two tmp-scroll-trigger tmp-fade-in animation-order-4">
+                  <a
+                    className="tmp-btn hover-icon-reverse radius-round download-icon"
+                    href="/assets/pdf/Maureen Njihia.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="icon-reverse-wrapper">
+                      <span className="btn-text">Download Resume</span>
+                      <span className="btn-icon">
+                        <i className="fa-regular fa-arrow-down" />
+                      </span>
+                      <span className="btn-icon">
+                        <i className="fa-regular fa-arrow-down" />
+                      </span>
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
