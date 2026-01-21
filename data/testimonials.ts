d63c1cf7ec3ca806d45c4562c49d86422932f9a0
Maureen Njihia <maureennjihia468@gmail.com>
2026-01-21 14:51:04 +0300

    feat: Implement a new Testimonials section using Swiper and update section spacing.

---
+++ data/testimonials.ts
@@ -0,0 +1,27 @@
+
+export const testimonials3 = [
+  {
+    name: "Angello Obel",
+    role: "CTO, Fresh Networks",
+    text: "",
+    animationOrder: "1",
+  },
+  {
+    name: "Dennis Kiptanui",
+    role: "CEO, Athens AI",
+    text: "Maureen is a proactive and collaborative team member whose positive attitude consistently enhances both team work and the quality of work. She is a genuine pleasure to work with and makes a strong, positive impact on any project.",
+    animationOrder: "2",
+  },
+  {
+    name: "Joanne Gitari",
+    role: "Lead Developer, Rhokit",
+    text: "Maureen is a skilled developer who delivers clean, responsive interfaces with meticulous attention to detail. Her professionalism and collaborative approach made working together at Rhokit a genuinely positive experience.",
+    animationOrder: "3",
+  },
+   {
+    name: "Erick Karanja",
+    role: "Director, Three Modern Systems Ltd",
+    text: "",
+    animationOrder: "4",
+  },
+];
