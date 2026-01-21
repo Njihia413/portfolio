1a5b5a2a1168dca00f52fdd70910c85705e906b7
Maureen Njihia <maureennjihia468@gmail.com>
2026-01-21 14:51:42 +0300

    feat: Add testimonials section and integrate Swiper.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/ui/header";
 import Hero from "@/components/hero";
 import Facts from "@/components/facts";
+import Testimonials from "@/components/testimonials";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Facts />
+      <Testimonials />
     </>
   );
 }
