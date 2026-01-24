a3d4dc94927bd385d0638a857b0b400d0fd86b8e
Maureen Njihia <maureennjihia468@gmail.com>
2026-01-24 12:37:01 +0300

    feat: Allow remote images from postimg.cc, integrate the Work component into the homepage, and update the favicon.

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Facts from "@/components/facts";
 import Testimonials from "@/components/testimonials";
 import Experience from "@/components/experience";
 import Skills from "@/components/skills";
+import Work from "@/components/work";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Testimonials />
       <Experience />
       <Skills />
+      <Work />
     </>
   );
 }
