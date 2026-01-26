daa938b7333a55f71a0c895b7d2e83c6ae9babf1
Maureen Njihia <maureennjihia468@gmail.com>
2026-01-26 13:09:15 +0300

    Refactor: Improve footer layout and `ScrollTop` placement, and standardize experience section styling with utility classes.

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,16 +10,15 @@ export default function Footer() {
             <div className="container">
               <div className="row align-items-center">
                 <div className="col-lg-12">
-                  <div className="copyright-right d-flex justify-content-between align-items-center">
-                    <div style={{ width: "40px" }} className="d-none d-lg-block"></div>
-                    <p className="copyright-text mb-0 text-center grow">
+                  <div className="copyright-right text-center">
+                    <p className="copyright-text mb-0">
                      Copyright © {new Date().getFullYear()} <span className="color-primary">Maureen Njihia</span> All rights reserved.
                     </p>
-                    <ScrollTop />
                   </div>
                 </div>
               </div>
             </div>
+            <ScrollTop />
           </div>
         </div>
       </div>{" "}
